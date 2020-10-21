ff0e7f919f6bd242950c10183c663d76415d0cca
Sevil2005 <sevillerasulova@gmail.com>
2020-10-21 15:34:47 +0400

    third commit

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,9 @@
 import os
 import smtplib
 import secrets
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from PIL import Image
@@ -11,31 +11,12 @@ from PIL import Image
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-posts = [
-    {
-        'author': "Sevil Rasulova",
-        'title': "Post 1",
-        'content': "Post 1 Content",
-        'date': "26-Sep"
-    },
-    {
-        'author': "Anar Rzayev",
-        'title': "Post 2",
-        'content': "Post 2 Content",
-        'date': "27-Sep"
-    },
-    {
-        'author': "Zəhra Bayramlı",
-        'title': "Post 3",
-        'content': "Post 3 Content",
-        'date': "28-Sep"
-    }
-]
-
 
 @app.route("/")
 @app.route("/home")
 def home():
+    # page = request.args.get('page', 1, type = int)
+    posts = Post.query.order_by(Post.date_posted.desc())
     return render_template('home.html', posts=posts)
 
 
@@ -141,9 +122,67 @@ def materials():
 
 @app.route('/məsləhət-bloqu')
 def blog():
+    posts = Post.query.all()
     return render_template('blog.html', title="Məsləhət Bloqu", posts=posts)
 
 
 @app.route('/müzakirə')
 def discuss():
     return render_template('discuss.html', title="Müzakirə")
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post( title = form.title.data, content = form.content.data, author = current_user )
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title="Yeni Məqalə", form=form, legend = 'Yeni Məqalə')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method =='GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title="Məqaləni Yenilə", form=form, legend = 'Məqaləni Yenilə')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type = int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc())
+    return render_template('user_posts.html', posts=posts, user=user)
